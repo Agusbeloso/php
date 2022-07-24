23883e94c55aa9d6504c990ac2d821fdb2dfecbd
Agusbeloso <belosoan@gmail.com>
2022-07-23 22:00:19 -0300

    cambios

--- contar.php
+++ contar.php
@@ -4,6 +4,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
+
+//Definicion
+function contar($aArray) {
+
+    $contador = 0;
+    foreach($aArray as $item){
+        $contador++;
+    }
+    return $contador;
+}
+
+ //Uso 
+$aNotas = array (9,8,9.50,4,7,8);    
+
+    
 $aPacientes = array();
 $aPacientes[] = array(
     "dni" => "33.765.012",
@@ -24,4 +39,29 @@ $aPacientes[] = array(
     "peso" => 77,);
 $aNotas = array(9, 8, 9.50, 4, 7, 8);
 
-?>
+$aProductos = array();
+$aProductos[] = array(
+    "nombre" => "Smart TV 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000,
+);
+$aProductos[] = array(
+    "nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000,
+);
+$aProductos[] = array(
+    "nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
+    "marca" => "Surrey",
+    "modelo" => "553AIQ1201E",
+    "stock" => 5,
+    "precio" => 45000,
+);
+
+echo "<br>Cantidad de productos: " . contar($aProductos); 
+echo "<br>Cantidad de pacientes: " . contar($aPacientes); 
+echo "<br>Cantidad de notas: " . contar($aNotas); 
