929a82957a99fb9e16942674344b05d12adc60b4
Agusbeloso <belosoan@gmail.com>
2022-07-24 21:01:01 -0300

    cambios

---
+++ funcion_maximo.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function maximo($aVector){
+    $maximo = 0;
+    foreach($aVector as $item){
+        if($maximo < $item){
+            $maximo = $item;
+        }
+    }
+    return $maximo;
+}
+
+//Uso
+$aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
+echo "El sueldo máximo es: " . maximo($aSueldo);
---
+++ funcion_promediar.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function promediar($aNumeros){
+    $suma = 0;
+    foreach ($aNumeros as $numero) {
+        $suma = $suma += $numero;
+    }
+    return $suma / count($aNumeros);
+}
+
+
+//Uso
+$aNotas = array(8, 4, 5, 3, 9, 1);
+echo "La nota promedio es: " . promediar($aNotas);
+
+
